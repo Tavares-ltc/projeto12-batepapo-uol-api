08303cb0bcf6fa1dc756e6b431c7e2ea8a537259
Lucas Tavares <l.tavaresc@hotmail.com>
2022-09-02 12:20:21 -0300

    Feita a função de postagem das mensagens no banco de dados.

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
+import dayjs from 'dayjs';
+import joi from 'joi'
 dotenv.config();
 
 const app = express();
@@ -16,18 +18,100 @@ mongoClient.connect().then(() => {
     db = mongoClient.db('batepapo');
 })
 
-app.post("/participants", async (req, res) => {
-    try {
-        db.collection('participants').insertOne(req.body)
-        res.sendStatus(200)
-
-    } catch (error) {
-        console.error(error)
-        res.sendStatus(422)
+async function isNameAvaliable(name) {
+    const someone = await db.collection('participants').findOne({name: name})
+    if (someone) {
+        console.log('ja tem')
+        return false
     }
+    console.log('não tem')
+    return true
+}
 
+app.post("/participants", async (req, res) => {
+    
+    const userSchema = joi.object({
+        name: joi.string().required().empty()
+    })
+    
+    const validation = userSchema.validate(req.body)
+    if (validation.error) {
+        return res.status(422).send({error: validation.error.message})
+    }
+    const name = req.body.name
+
+    if (await isNameAvaliable(name)) {
+        const data = {
+            name,
+            lastStatus: Date.now()
+        }
+        const loginMessage = { from: name, to: 'Todos', text: 'entra na sala...', type: 'status', time: `${dayjs().hour()}:${dayjs().minute()}:${dayjs().second()}` }
+        try {
+            await db.collection('participants').insertOne(data)
+            await db.collection('messages').insertOne(loginMessage)
+            return res.sendStatus(200)
+
+        } catch (error) {
+            console.error(error)
+            return res.sendStatus(422)
+        }
+    }
+    return res.sendStatus(409)
 })
 
+app.get("/participants", async (req, res)=> {
+    try {
+        const participants = await db.collection('participants').find({}).toArray()
+        return res.status(200).send(participants)
+
+    } catch (error) {
+
+        console.log(error)
+        return res.sendStatus(400)
+    }
+    
+})
+
+app.post("/messages", async (req, res)=> {
+    const messageSchema = joi.object({
+        to: joi.string().required().empty(),
+        text: joi.string().required().empty(),
+        type: joi.string().required() //type só pode ser 'message' ou 'private_message'
+    })
+
+    const validation = messageSchema.validate(req.body)
+    
+    if(validation.error) {
+        return res.status(422).send({error: validation.error})
+    }
+
+    try {
+      const someone =  await db.collection('participants').find({name: req.headers.user}).toArray()
+      console.log(someone)
+      if(someone.length === 0){
+        return res.status(404).send('O remetente não existe.')
+      }
+    } catch (error) {
+        console.log(error)
+        return res.sendStatus(422)
+    }
+
+    const dataMessage = {
+        to: req.body.to,
+        text: req.body.text,
+        type: req.body.type,
+        from: req.headers.user,
+        time: `${dayjs().hour()}:${dayjs().minute()}:${dayjs().second()}`
+    }
+
+    try {
+        await db.collection('messages').insertOne(dataMessage)
+        res.sendStatus(201)
+    } catch (error) {
+        console.log(error)
+        res.sendStatus(422)
+    }
+})
 
 app.listen(5000, () => {
     console.log('listen on port 5000')
